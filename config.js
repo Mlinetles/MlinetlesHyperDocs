b3d0c17567f3142cc6f1718d81fbe03ec85cb2b2
Mliybs <2491988463@qq.com>
2024-01-27 12:34:18 +0800

    updated on : 2024-01-27 12:34:18

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    title: 'MlinetlesHyper',
+    description: 'MlinetlesHyper的文档网站',
+    themeConfig: {
+        nav: [
+            {
+                text: 'mnd相关',
+                items: [
+                    { text: 'mnd介绍', 'link': '/mnd/' }
+                ]
+            }
+        ],
+        sidebar: {
+            '/mnd/': [
+                ['', 'mnd文件'],
+                ['format', 'mnd格式']
+            ],
+            '/': [
+                ['', '首页'],
+                ['/mnd/', 'mnd文件']
+            ]
+        }
+    }
+}
