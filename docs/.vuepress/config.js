1acd1dfee0e474274bf73403361d254f4e8fb85a
Mliybs <2491988463@qq.com>
2024-01-27 09:45:32 +0800

    ?

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+﻿module.exports = {
+    title: '原神，启动！',
+    description: '乐',
+    themeConfig: {
+        nav: [
+            text: 'mnd相关',
+            items: [
+                { text: 'mnd介绍', 'link': '/mnd/intro' }
+            ]
+        ]
+    }
+}
